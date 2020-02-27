917504c82f9185cd8f2a9174ba883222a92ae7f6
Esteban Arocha <e.arocha@ukko.mx>
2020-02-26 23:01:36 -0600

    Create home Payment model

--- app/src/main/java/com/example/epesos/home/model/Company.kt
+++ app/src/main/java/com/example/epesos/home/model/Company.kt
@@ -1,15 +1,11 @@
 package com.example.epesos.home.model
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class Service (
+data class Company (
     var supplier_id : String,
     var category :String,
     var name :String,
     var url_logo :String
-) : Parcelable {
+) {
     constructor() : this(
         supplier_id = "",
         category = "",
--- app/src/main/java/com/example/epesos/home/model/PaymentInteractor.kt
+++ app/src/main/java/com/example/epesos/home/model/PaymentInteractor.kt
@@ -1,4 +1,55 @@
 package com.example.epesos.home.model
+import android.content.Context
+import java.io.InputStream
 
 class PaymentInteractor {
+    companion object {
+        private val TAG: String = PaymentInteractor::class.java.simpleName
+    }
+
+    interface OnFinishedListener {
+        fun onResultSuccessPhoneServices(json :String)
+        fun onResultSuccessServices(json :String)
+        fun onResultSuccessCommunicationServices(json :String)
+        fun onResultFail(strError: String)
+    }
+
+    fun getPhoneServices(filename :String, context: Context, onFinishedListener: OnFinishedListener){
+        val phoneServices = readJSONFromAsset(filename, context)
+        if (phoneServices != "error") {
+            onFinishedListener.onResultSuccessPhoneServices(phoneServices)
+        } else {
+            onFinishedListener.onResultFail("Failed to retrieve json.")
+        }
+    }
+
+    fun getServices(filename :String, context: Context, onFinishedListener: OnFinishedListener){
+        val services = readJSONFromAsset(filename, context)
+        if (services != "error") {
+            onFinishedListener.onResultSuccessServices(services)
+        } else {
+            onFinishedListener.onResultFail("Failed to retrieve json.")
+        }
+    }
+
+    fun getCommunicationServices(filename :String, context: Context, onFinishedListener: OnFinishedListener){
+        val phoneServices = readJSONFromAsset(filename, context)
+        if (phoneServices != "error") {
+            onFinishedListener.onResultSuccessCommunicationServices(phoneServices)
+        } else {
+            onFinishedListener.onResultFail("Failed to retrieve json.")
+        }
+    }
+
+    fun readJSONFromAsset(filename :String, context: Context) : String {
+        var json: String? = null
+        try {
+            val  inputStream: InputStream = context.assets.open(filename)
+            json = inputStream.bufferedReader().use{it.readText()}
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            return "error"
+        }
+        return json
+    }
 }
